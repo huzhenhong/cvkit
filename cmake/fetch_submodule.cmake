a034766e0696eb16f2af34583926aa27b0ec3985
huzhenhong <455879568@qq.com>
2023-12-13 12:15:46 +0800

    optimize cmake file

--- cmake/fetch_submodule.cmake
+++ cmake/fetch_submodule.cmake
@@ -7,6 +7,6 @@ FetchContent_Declare(
     QUIET
     GIT_REPOSITORY https://github.com/huzhenhong/${LIB_NAME}.git
     # URL https://github.com/open-source-parsers/jsoncpp/archive/1.9.4.tar.gz)
-    GIT_TAG main
+    GIT_TAG 35154c9e7afb76fcc53e30151925df861c0b115e
     SOURCE_DIR ${CMAKE_SOURCE_DIR}/ext/${LIB_NAME})
 FetchContent_MakeAvailable(${LIB_NAME})
